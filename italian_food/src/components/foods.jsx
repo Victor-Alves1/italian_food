0c9938ac311a196fe7c5e3704f27b87abd59b2c6
Victor Alves da Costa <victoralves741@gmail.com>
2022-05-27 12:21:11 -0300

    image update 2022 may 27

--- italian_food/src/components/foods.jsx
+++ italian_food/src/components/foods.jsx
@@ -9,27 +9,113 @@ import salada from '../../images/salada.jpg'
 const Foods = () => {
     return (
         <div className='flex'>
-            <div className='w-comida'>
-                <img src={massa} alt="" />
-            </div>
-            <div className='w-comida'>
-                <img src={couvert} alt="" />
-            </div>
-            <div className='w-comida'>
-                <img src={bolinho} alt="" />
-            </div>
-            <div className='w-comida'>
-                <img src={macarrao} alt="" />
-            </div>
-            <div className='w-comida'>
-                <img src={mistura} alt="" />
-            </div>
-            <div className='w-comida'>
-                <img src={pizza} alt="" />
-            </div>
-            <div className='w-comida'>
-                <img src={salada} alt="" />
-            </div>
+            
+            <input type="radio" name="info-comida" id="menu1" />
+            <input type="radio" name="info-comida" id="menu2" />
+            <input type="radio" name="info-comida" id="menu3" />
+            <input type="radio" name="info-comida" id="menu4" />
+            <input type="radio" name="info-comida" id="menu5" />
+            <input type="radio" name="info-comida" id="menu6" />
+            <input type="radio" name="info-comida" id="menu7" />
+
+            <label htmlFor="menu1">
+                <div className='w-comida'>
+                    <img src={massa} alt="" />
+                    <div className='info-comida'>
+                        <h1>
+                            Lorem ipsum
+                        </h1>
+                        <br />
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, pariatur eveniet accusamus facilis eaque amet accusantium mollitia perferendis! Molestiae ipsum sunt dicta tempore natus aliquam obcaecati, ullam temporibus eaque recusandae.
+                        </p>
+                    </div>
+                </div>
+            </label>
+            <label htmlFor="menu2">
+                <div className='w-comida'>
+                    <img src={couvert} alt="" />
+                    <div className='info-comida'>
+                        <h1>
+                            Lorem ipsum
+                        </h1>
+                        <br />
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, pariatur eveniet accusamus facilis eaque amet accusantium mollitia perferendis! Molestiae ipsum sunt dicta tempore natus aliquam obcaecati, ullam temporibus eaque recusandae.
+                        </p>
+                    </div>
+                </div>
+            </label>
+            <label htmlFor="menu3">
+                <div className='w-comida'>
+                    <img src={bolinho} alt="" />
+                    <div className='info-comida'>
+                        <h1>
+                            Lorem ipsum
+                        </h1>
+                        <br />
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, pariatur eveniet accusamus facilis eaque amet accusantium mollitia perferendis! Molestiae ipsum sunt dicta tempore natus aliquam obcaecati, ullam temporibus eaque recusandae.
+                        </p>
+                    </div>
+                </div>
+            </label>
+            <label htmlFor="menu4">
+                <div className='w-comida'>
+                    <img src={macarrao} alt="" />
+                    <div className='info-comida'>
+                        <h1>
+                            Lorem ipsum
+                        </h1>
+                        <br />
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, pariatur eveniet accusamus facilis eaque amet accusantium mollitia perferendis! Molestiae ipsum sunt dicta tempore natus aliquam obcaecati, ullam temporibus eaque recusandae.
+                        </p>
+                    </div>
+                </div>
+            </label>
+            <label htmlFor="menu5">
+                <div className='w-comida'>
+                    <img src={mistura} alt="" />
+                    <div className='info-comida'>
+                        <h1>
+                            Lorem ipsum
+                        </h1>
+                        <br />
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, pariatur eveniet accusamus facilis eaque amet accusantium mollitia perferendis! Molestiae ipsum sunt dicta tempore natus aliquam obcaecati, ullam temporibus eaque recusandae.
+                        </p>
+                    </div>
+                </div>
+            </label>
+            <label htmlFor="menu6">
+                <div className='w-comida'>
+                    <img src={pizza} alt="" />
+                    <div className='info-comida'>
+                        <h1>
+                            Lorem ipsum
+                        </h1>
+                        <br />
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, pariatur eveniet accusamus facilis eaque amet accusantium mollitia perferendis! Molestiae ipsum sunt dicta tempore natus aliquam obcaecati, ullam temporibus eaque recusandae.
+                        </p>
+                    </div>
+                </div>
+            </label>
+            <label htmlFor="menu7">
+                <div className='w-comida'>
+                    <img src={salada} alt="" />
+                    <div className='info-comida'>
+                        <h1>
+                            Lorem ipsum
+                        </h1>
+                        <br />
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, pariatur eveniet accusamus facilis eaque amet accusantium mollitia perferendis! Molestiae ipsum sunt dicta tempore natus aliquam obcaecati, ullam temporibus eaque recusandae.
+                        </p>
+                    </div>
+                </div>
+            </label>
         </div>
     );
 }
